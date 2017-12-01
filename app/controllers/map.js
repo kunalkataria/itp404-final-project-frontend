0bb6ba40ac6529140c12abc072584c42ff376a79
Kunal Kataria <kkataria@usc.edu>
2017-11-30 23:20:53 -0800

    making progress

--- app/controllers/map.js
+++ app/controllers/map.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    handleClick(restaraunt) {
+      console.log(restaraunt.location);
+      this.set('details', restaraunt.name);
+    }
+  }
 });
--- app/routes/map.js
+++ app/routes/map.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return $.ajax({
+      type: 'GET',
+      url: 'http://localhost:3000/api/favorites',
+    });
+  }
 });
