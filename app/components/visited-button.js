5635fb34a2a1e67ebeb334d2129beea91945b159
Kunal Kataria <kkataria@usc.edu>
2017-12-01 02:02:49 -0800

    loading indicator

--- app/components/visited-button.js
+++ app/components/visited-button.js
@@ -5,6 +5,7 @@ export default Ember.Component.extend({
     let visited = this.get('visited');
     let id = this.get('id');
     console.log(id);
+    this.set('loading', true);
     $.ajax({
       type: 'PATCH',
       url: `http://localhost:3000/api/favorites/${id}`,
@@ -12,7 +13,13 @@ export default Ember.Component.extend({
         visited: !visited,
       }
     }).then(function(response) {
-      this.set('visited', !visited);
+      /* This run later slows down the actual setting so it makes it look like
+        the code is taking longer than actual to make user feel that the change
+        is more permanent */
+      Ember.run.later((function() {
+        this.set('visited', !visited);
+        this.set('loading', false);
+      }.bind(this)), 500);
     }.bind(this), function(response) {
       toastr.error('something went wrong, was not able to switch visit status');
     });
