a8499cbee8ff037d780dc5a6caebe9bd7bf9561b
Kunal Kataria <kkataria@usc.edu>
2017-12-01 17:59:05 -0800

    first acceptance test

--- app/components/delete-button.js
+++ app/components/delete-button.js
@@ -7,7 +7,11 @@ export default Ember.Component.extend({
       type: 'DELETE',
       url: `http://localhost:3000/api/favorites/${id}`,
     }).then(function(response) {
+      /* will force the page to refresh and get the new list without the element
+        that was just deleted */
       window.location.reload(true);
+    }, function(response) {
+      toastr.error('Something went wrong when trying to delete');
     });
   }
 });
---
+++ tests/acceptance/search-test.js
@@ -0,0 +1,25 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'final-project/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | search');
+
+// default generated test
+/* test('visiting /search', function(assert) {
+  visit('/search');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/search');
+  });
+
+}); */
+
+test('create new favorite restaraunt', function(assert) {
+  visit('/search');
+  fillIn('#restaraunt', 'Bestia');
+  fillIn('#location', '2121 E 7th Pl, Los Angeles, CA 90021')
+  click('button.submit');
+  andThen(function() {
+    assert.equal('input.restaraunt', 'Bestia');
+    assert.equal('input.restaraunt', '2121 E 7th Pl, Los Angeles, CA 90021');
+  });
+});
